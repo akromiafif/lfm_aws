f538658cfe0ee1a4850f163399007f93634b5d38
Afif Akromi <massachusetts48@gmail.com>
2021-08-06 10:13:09 +0700

    add register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,31 +4,63 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
-    
     public function register(Request $request)
     {
-        $this->validate($request, [
+        $user = new User();
+
+        $rules = array (
             'nama' => 'required|min:4',
             'domisili' => 'required',
             'username' => 'required|min:6',
             'email' => 'required|email',
             'password' => 'required|min:8',
-        ]);
- 
-        $user = User::create([
-            'nama' => $request->nama,
-            'domisili' => $request->domisili,
-            'email' => $request->email,
-            'username' => $request->username,
-            'password' => bcrypt($request->password)
-        ]);
-       
-        $token = $user->createToken('lfmAuthApp')->accessToken;
- 
-        return response()->json(['user' => $user, 'token' => $token], 200);
+        );
+
+        $validated = Validator::make($request->all(), $rules);
+        $userExist = User::where('username', $request->input('username'))->first();
+        $emailExist = User::where('email', $request->input('email'))->first();
+
+        $resArr = [];
+        if ($userExist != null) {
+            
+            $resArr['status'] = false;
+            $resArr['message'] = 'Username already exist';
+
+            return response()->json($resArr, 200);
+        }
+
+        if ($emailExist != null) {
+            $resArr['status'] = false;
+            $resArr['message'] = 'Email already exist';
+
+            return response()->json($resArr, 200);
+        } 
+        
+        if ($validated->fails()){
+            $error = $validated->messages();
+            return response()->json($error, 200);
+        }
+        else {
+            $user->nama = $request->input('nama');
+            $user->domisili = $request->input('domisili');
+            $user->username = $request->input('username');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            
+            $resArr = [];
+            $resArr['token'] = $user->createToken('token')->accessToken;
+            $resArr['username'] = $user->username;
+            $resArr['status'] = true;
+
+            $user->save();
+            return response()->json($resArr, 200);
+        }
     }
  
     public function login(Request $request)
@@ -39,8 +71,10 @@ class AuthController extends Controller
         ];
  
         if (auth()->attempt($data)) {
+            $user = User::where('email', $data['email'])->first();
+
             $token = auth()->user()->createToken('lfmAuthApp')->accessToken;
-            return response()->json(['status' => true, 'message' => ['token' => $token]], 200);
+            return response()->json(['status' => true, 'message' => ['email' => $user['email'], 'token' => $token]], 200);
         } else {
             return response()->json(['status' => false, 'message' => 'Invalid credentials'], 200);
         }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::get('profile/{email}', [UserController::class, 'user']);
